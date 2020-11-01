98f26ff94c4e18a6544bb6d28372d164a2753fad
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-11-01 20:01:14 +0100

    need to add ssetTimeout function and improved the footer  element

--- index.js
+++ index.js
@@ -8,3 +8,34 @@ function flipCard() {
 
 //listening to the click event
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+//adding my name into and H1
+const bg = document.querySelector('.name');
+const title = document.createElement('H1');
+title.innerText = 'Juan Fco Paredes';
+bg.appendChild(title);
+
+//setInterval function
+
+const techStack = ['React', 'Git', 'Boostrap', 'React Hooks', 'firebase', 'HTML', 'CSS', 'JS'];
+let text = "";
+
+function iteration() {
+
+  setTimeout(function (){
+
+    for(let i = 0; i < techStack.length; i++) {
+      text += techStack[i] + "<br>";
+    };
+      const stack = document.querySelector('.techStack');
+      stack.innerHTML =  text;
+
+  },2000)
+
+
+
+}
+iteration();
+
