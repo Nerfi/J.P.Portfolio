4632d24bae6cc648b9f284b35af5de45d84f52aa
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-12-18 19:58:50 +0100

    media querys needs to be done

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 
 
 
-//adding my name into and H1
-const bg = document.querySelector('.name');
-const title = document.createElement('H1');
-title.innerText = 'Juan Fco Paredes';
-bg.appendChild(title);
-
-
 //setInterval function
 const techStack = ['React', 'Git', 'Boostrap', 'React Hooks', 'firebase', 'HTML', 'CSS', 'JS'];
 let text = "";
